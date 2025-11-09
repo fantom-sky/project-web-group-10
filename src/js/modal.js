0fc8ddd4ca9a7d3dfc2014b4b70f69fd567a46a8
I.Kumpan <ilyakumpan11@gmail.com>
2025-11-09 15:43:17 +0200

    upd modal js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,18 +1,16 @@
 (() => {
   const refs = {
-    // Елемент форми, який потрібно прослуховувати
     subscribeForm: document.querySelector('.subscribe-form'),
-    // Додати атрибут data-modal-close на кнопку закриття (залишається)
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    // Додати атрибут data-modal на бекдроп модалки (залишається)
+    closeModalBtns: document.querySelectorAll('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-    // Поле введення email
     emailInput: document.getElementById('subscribe-email'),
   };
 
-
   refs.subscribeForm.addEventListener('submit', handleFormSubmit);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  refs.closeModalBtns.forEach(btn => {
+    btn.addEventListener('click', toggleModal);
+  });
 
   function toggleModal() {
     refs.modal.classList.toggle('is-open');
@@ -20,6 +18,7 @@
 
   function handleFormSubmit(event) {
     event.preventDefault(); 
+    
     if (refs.emailInput.checkValidity()) {
       toggleModal();
     } else {
